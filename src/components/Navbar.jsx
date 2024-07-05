dd7f88f3619d8f14ed985f6f3607350bfa890c8b
Gabriel <wrangleronline@hotmail.com>
2024-07-05 15:00:13 +0200

    Navbar made responsive now working on the main section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { FaHome } from "react-icons/fa";
+import { RiAdminFill } from "react-icons/ri";
 export default function Navbar() {
   return (
-    <nav className="bg-slate-950 h-16 mb-10">
-      <ul className="flex flex-row justify-end items-center h-full">
-        <li
-          className=" nav-link"
-        >
-          <Link to={"/"}>Accueil</Link>
+    <nav className="bg-slate-950 p-5 md:navbar-md sticky" >
+      <div className="hidden md:inline">
+        <span className="text-xl cursor-pointer md:text-4xl pl-16 md:pl-0">
+          <img
+            className="h-10 md:h-20 rounded-full mr-5 inline"
+            src="assets/images/profile_pic.png"
+          />
+          Portfolio
+        </span>
+      </div>
+      <ul className="flex items-center justify-around md:justify-end bg-slate-950">
+        <li className="mx-4 my-6 md:my-0">
+          <Link to={"/"} className="text-xl hover:text-[#993125] duration-200">
+            Accueil
+          </Link>
         </li>
-        <li
-          className="nav-link"
-        >
-          <Link to={"/admin"}>Admin Page</Link>
+        <li className="mx-4 my-6 md:my-0">
+          <Link
+            to={"/admin"}
+            className="text-xl hover:text-[#993125] duration-200"
+          >
+            Admin Page
+          </Link>
         </li>
       </ul>
     </nav>
   );
 }
+
+
