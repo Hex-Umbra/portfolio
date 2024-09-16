8ea8b130a87e4a75cd7ace113204a2fccf4a576d
Hex-Umbra <g.p.dacosta@hotmail.com>
2024-09-16 11:18:44 +0200

    Changed the Formation Section, deleted the Projects for now as I don't really have something concrete to show yet

--- src/components/Formations.jsx
+++ src/components/Formations.jsx
@@ -4,7 +4,7 @@ import InfoExperiences from './InfoExperiences'
 
 export default function Formations() {
   return (
-    <section className="h-auto bg-[#79261d] md:my-10 shadow-2xl shadow-black md:rounded-2xl md:mx-4">
+    <section className="h-auto bg-[#79261d] md:my-10">
         <div className="grid md:grid-cols-2 justify-items-center h-full relative">
             <div className="w-full"><InfoFormations/></div>
             <div className="w-full"><InfoExperiences/></div>
